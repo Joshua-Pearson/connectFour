768875b153551993835af6fca9daac5b48471e47
Joshua-Pearson <j_d_pearson@yahoo.com>
2014-08-08 01:46:12 -0700

    working on animations with click

--- public/javascripts/app-game.js
+++ public/javascripts/app-game.js
@@ -7,15 +7,40 @@
       restrict: "E",
       templateUrl: "partials/main-game.ejs",
       controller: function() {
-        this.hiThere = "HI HI HI HI HIH I HIHI";
+         this.moveCount = 0;
+         var _this = this;       
 
-        this.isSet = function(checkTab) {
-          return "This is a " + checkTab + " function";
+        this.move = function(square) {
+          if (currentPlayer === playerOne && square.isPlayed === false) {
+            square.isPlayed = true;
+            square.color = currentPlayer.value;
+            currentPlayer = playerTwo;
+          } else if (currentPlayer === playerTwo && square.isPlayed === false)  {
+            square.isPlayed = true;
+            square.color = currentPlayer.value;
+            currentPlayer = playerOne;
+          } else {
+            alert("illegal move")
+          }
         };
 
-        this.setTab = function(activeTab) {
-          this.tab = activeTab;
+        function player(playerNumber, value) {
+          this.playerNumber = playerNumber;
+          this. value = value;
         };
+
+        var playerOne = new player("One", "red")
+        var playerTwo = new player("Two", "blue")
+
+        var currentPlayer = playerOne;
+
+        this.columnOne = [{"row": 1, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 2, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 3, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 4, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 5, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 6, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 7, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}];
+        this.columnTwo = [{"row": 1, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 2, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 3, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 4, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 5, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 6, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 7, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}];
+        this.columnThree = [{"row": 1, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 2, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 3, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 4, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 5, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 6, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 7, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}];
+        this.columnFour = [{"row": 1, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 2, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 3, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 4, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 5, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 6, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 7, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}];
+        this.columnFive = [{"row": 1, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 2, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 3, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 4, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 5, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 6, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 7, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}];
+        this.columnSix = [{"row": 1, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 2, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 3, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 4, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 5, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 6, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}, {"row": 7, "value" : "O", "color": "blank", "isPlayed": false, "nothing": ""}];
+
       },
       controllerAs: "game"
     };
